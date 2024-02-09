50785d9afc7aa6aac7a385f39ebde8e416158f72
ludsonfelipe <felipe_ludson@hotmail.com>
2024-02-09 16:47:04 -0300

    adding more details

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
 
 ;; Definição de repositórios
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa/")
-                         ("elpa"  . "https://elpa.gnu.org/packages/")))
+                        ("org"   . "https://orgmode.org/elpa/")
+                        ("elpa"  . "https://elpa.gnu.org/packages/")))
 
 ;; Inicialização do sistema de pacotes
 (package-initialize)
@@ -35,13 +35,8 @@
  '(custom-safe-themes
    '("fd1fea4311568a0da48c2e05d1efbb07d31ea625eedb289249156a0d9a115c8f" default))
  '(package-selected-packages
-   '(auto-package-update neotree elpy projectile dashboard rainbow-mode doom-themes all-the-icons-dired all-the-icons use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(doom-modeline elfeed-goodies elfeed org-ai auto-package-update neotree elpy projectile dashboard rainbow-mode doom-themes all-the-icons-dired all-the-icons use-package)))
+
 
 ;; Desativando Menu
 (menu-bar-mode -1)
@@ -91,7 +86,7 @@
                           (registers . 3)))
   :custom 
   (dashboard-modify-heading-icons '((recents . "file-text")
-				    (bookmarks . "book")))
+            (bookmarks . "book")))
   :config
   (dashboard-setup-startup-hook))
 
@@ -107,6 +102,7 @@
 ; Tree folders lateral
 (use-package neotree)
 (setq neo-theme (if (display-graphic-p) 'icons))
+(setq neo-window-width 35)
 
 ;; Linha em highlight
 (global-hl-line-mode t)
@@ -125,13 +121,35 @@
 
 (global-set-key [f6] 'transwin-toggle)
 (global-set-key [f7] 'neotree-toggle)
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-x f") 'elfeed)
 
 ;; Jupyter Notebook + Ipython
 (setq python-shell-interpreter "jupyter"
       python-shell-interpreter-args "console --simple-prompt"
       python-shell-prompt-detect-failure-warning nil)
 (add-to-list 'python-shell-completion-native-disabled-interpreters
-             "jupyter")
+            "jupyter")
+
+;; feed do reddit
+(use-package elfeed
+  :config
+  (setq elfeed-search-feed-face ":foreground #ffffff :weight bold"
+        elfeed-feeds (quote
+                       (("https://www.reddit.com/r/dataengineering.rss" reddit data_engineer)
+                        ("https://www.reddit.com/r/emacs.rss" reddit emacs)
+			("https://www.reddit.com/r/Python.rss" reddit python)
+			("https://www.reddit.com/r/scala.rss" reddit scala)
+			("https://www.reddit.com/r/SQL.rss" reddit sql)
+			))))
+ 
+
+(use-package elfeed-goodies
+  :init
+  (elfeed-goodies/setup)
+  :config
+  (setq elfeed-goodies/entry-pane-size 0.5))
 
 ;; Instalação do auto-update
 (use-package auto-package-update
@@ -143,6 +161,38 @@
   (auto-package-update-maybe)
   (auto-package-update-at-time "21:00"))
 
+(use-package org-ai
+  :ensure t
+  :commands (org-ai-mode
+             org-ai-global-mode)
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode) ; enable org-ai in org-mode
+  (org-ai-global-mode) ; installs global keybindings on C-c M-a
+  :config
+  (setq org-ai-default-chat-model "gpt-3.5-turbo") ; if you are on the gpt-4 beta:
+  (setq org-ai-openai-api-token "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")
+  (org-ai-install-yasnippets)) ; if you are using yasnippet and want `ai` snippets
 
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
+;; moodline do footer
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :config
+  (setq doom-modeline-height 35)
+  (setq doom-modeline-bar-width 3)
+  (setq doom-modeline-hud nil)
+  (setq doom-modeline-project-detection 'auto)
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-persp-name t)   ;; adds perspective name to modeline
+  (setq doom-modeline-position-column-line-format '("%l:%c"))
+  ) 
+
+;;(use-package sudo-edit)
